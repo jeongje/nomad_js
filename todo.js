68d04f13cb94542daf11efed7583b582fff36ad4
jeongje <uonnow@gmail.com>
2020-02-04 10:02:53 +0900

    자동으로 목록 생성

--- todo.js
+++ todo.js
@@ -4,15 +4,31 @@ toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos"
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+
+
 function paintTodo(text){
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "❌";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "❌";
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 
@@ -25,9 +41,12 @@ function handleSubmit(event){
 
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-
+    const loadedtoDos = localStorage.getItem(TODOS_LS);
+    if(loadedtoDos !== null) {
+        const parsedToDos = JSON.parse(loadedtoDos);
+        parsedToDos.forEach(function(toDo){
+            paintTodo(toDo.text);
+        });
     }
 }
 
