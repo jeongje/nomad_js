71a44e0d41f91c2db990dbade77a9b2369f58623
jeongje <uonnow@gmial.com>
2020-02-08 14:51:08 +0900

    날씨출력

--- weather.js
+++ weather.js
@@ -1,7 +1,23 @@
+const weather = document.querySelector(".js-weather")
+
 const API_KEY = "4d389fec1fb7af73044b8d772c45de22";
 const COORDS = "coords";
 
 
+function getWeather(lat, lng){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+        )
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`
+        console.log(json)
+    });
+}
 
 
 function saveCoords(coordsObj){
@@ -18,6 +34,7 @@ function handleGeoSucces(position) {
         longitude,
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 
@@ -35,7 +52,8 @@ function loadCoords() {
     if(loadedCoords === null){
         askForCoords();
     } else {
-
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
